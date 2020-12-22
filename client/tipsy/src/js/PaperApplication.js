758cc31c246c47d5dd54abdcaecdd477de75dfc3
lev <levavakian@gmail.com>
2020-12-22 08:08:54 -0800

    resize

--- client/tipsy/src/js/PaperApplication.js
+++ client/tipsy/src/js/PaperApplication.js
@@ -16,22 +16,19 @@ export default class PaperApplication {
         let w = window.innerWidth;
         let h = window.innerHeight;
 
-        const canvas = document.createElement('canvas');
-        canvas.id = 'paper-canvas';
-        document.body.appendChild(canvas);
-
-        paper.setup(canvas);
+        paper.install(window)
+        paper.setup(document.getElementById('canvas'));
         
         paper.view.onFrame = function(event){
             path.rotate(3)
         }
 
-        console.log("1")
-        console.log(db)
-        console.log("1")
-
         var raster = new paper.Raster(db);
         raster.position = paper.view.center
+
+        paper.view.onResize = function(event) {
+            raster.position = paper.view.center
+        }
 
         var path = new paper.Path.Rectangle({
             point: [75, 75],
